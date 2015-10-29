94a7c1fbaf6c981ff4d2f540b8c3aa67a37cd6ea
ryym <ryym.64@gmail.com>
2015-10-29 18:50:50 +0900

    Improve: DSL1_A: Using Union-Find Tree

--- dsl/disjoint_set.cpp
+++ dsl/disjoint_set.cpp
@@ -1,105 +1,45 @@
 /* DSL1_A: Disjoint Set */
 
-// 連結リストによるぎこちない実装
+// Solve using Union-Find Tree
 
 #include <iostream>
 #include <cstdio>
 
-class Node {
-    public:
-        const int value;
-        Node* prev;
-        Node* next;
+int *parents;
+int *ranks;
 
-        Node(int value): value(value) {}
-        ~Node() {
-            delete prev;
-            delete next;
-        }
-};
-
-class LinkedList {
-    Node* guard;
-    int _size;
-
-    public:
-        const int id;
-
-        LinkedList(int id, Node* first): id(id) {
-            guard = new Node(-1);
-            guard->next = first;
-            guard->prev = first;
-            first->next = guard;
-            first->prev = guard;
-            _size = 1;
-        }
-
-        ~LinkedList() {
-            delete guard;
-        }
-
-        void append_all(LinkedList* other) {
-            last()->next = other->first();
-            other->first()->prev = last();
-
-            guard->prev  = other->last();
-            other->last()->next  = guard;
-
-            _size += other->size();
-        }
-
-        Node* first() {
-            return guard->next;
-        }
-
-        Node* last() {
-            return guard->prev;
-        }
-
-        bool is_end(Node* node) {
-            return node == guard;
-        }
-
-        int size() {
-            return _size;
-        }
-};
-
-LinkedList **SETs;
+int find(int x) {
+    if (parents[x] == x)
+        return x;
+    else
+        return parents[x] = find( parents[x] );
+}
 
 void unite(int x, int y) {
-    if ( SETs[x] == SETs[y] ) return;
-
-    bool is_x_bigger = SETs[x]->size() >= SETs[y]->size();
-    LinkedList* main = is_x_bigger ? SETs[x] : SETs[y];
-    LinkedList* sub  = is_x_bigger ? SETs[y] : SETs[x];
-
-    Node* node = sub->first();
-    while(! sub->is_end(node)) {
-        SETs[ node->value ] = main;
-        node = node->next;
+    x = find(x);
+    y = find(y);
+    if ( ranks[x] < ranks[y] ) {
+        parents[x] = y;
     }
-    main->append_all(sub);
-}
-
-bool same(int x, int y) {
-    return SETs[x]->id == SETs[y]->id;
-}
-
-void create_sets(int n) {
-    for (int i = 0; i < n; i++) {
-        Node *node = new Node(i);
-        LinkedList *set = new LinkedList(i, node);
-        SETs[i] = set;
+    else if ( ranks[y] < ranks[x] ) {
+        parents[y] = x;
+    }
+    else if (x != y) {
+        parents[x] = y;
+        ranks[y] += 1;
     }
 }
 
 int main() {
     int n, q;
-    scanf("%d %d\n", &n, &q);
+    scanf("%d %d", &n, &q);
 
-    SETs = new LinkedList*[n];
-    create_sets(n);
+    parents = new int[n];
+    ranks   = new int[n];
+    for (int i = 0; i < n; i++) {
+        parents[i] = i;
+        ranks[i]   = 0;
+    }
 
     int command, x, y;
     for (int i = 0; i < q; i++) {
@@ -109,10 +49,11 @@ int main() {
                 unite(x, y);
                 break;
             case 1:
-                printf("%d\n", same(x, y));
+                printf("%d\n", find(x) == find(y));
                 break;
         }
     }
+
     return 0;
 }
 
